25a8ecd57b02e838321f20311ec680a7147f9624
Kentin Fankhauser <pm27rlp@eduvaud.ch>
2026-03-19 09:03:49 +0100

    fix(new solution structure): a new solution was created beacuase the last one had version problems

--- src/MineSweeper_Kentin/Display/SuperConsole.cs
+++ src/MineSweeper_Kentin/Display/SuperConsole.cs
@@ -8,30 +8,6 @@ namespace MineSweeper.Display
 {
     public static class SuperConsole
     {
-        public static void DrawField(int x, int y, int width, int height)
-        {
-            // Ligne du haut
-            DrawHorizontalLine(x, y, width, '─');
-
-            // Ligne du bas
-            DrawHorizontalLine(x, y + height - 1, width, '─');
-
-            // Côté gauche
-            DrawVerticalLine(x, y, height, '│');
-
-            // Côté droit
-            DrawVerticalLine(x + width - 1, y, height, '│');
-
-            // Coins
-            Console.SetCursorPosition(x, y);
-            Console.Write('┌');
-            Console.SetCursorPosition(x + width - 1, y);
-            Console.Write('┐');
-            Console.SetCursorPosition(x, y + height - 1);
-            Console.Write('└');
-            Console.SetCursorPosition(x + width - 1, y + height - 1);
-            Console.Write('┘');
-        }
         public static void DrawAtString(int x, int y, string phrase)
         {
             Console.SetCursorPosition(x, y);
---
+++ src/MineSweeper_Kentin/Program.cs
@@ -0,0 +1,12 @@
+﻿using MineSweeper.Features;
+namespace MineSweeper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MineField field = new MineField();
+            field.DrawField(5, 5, 5, 5);
+        }
+    }
+}
